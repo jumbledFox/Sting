a2bfea1fb56ef6c6db8745a478dbd08ea41cc85b
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2023-05-04 22:59:08 +0100

    Finish... :3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use serde::Deserialize;
 use walkdir::WalkDir;
 use comrak::{markdown_to_html, ComrakOptions};
+use std::borrow::Borrow;
 use std::env;
 use std::fs;
+use std::collections::HashMap;
+use titlecase::titlecase;
 
 #[derive(Deserialize, Debug)]
 struct Page {
@@ -26,26 +29,52 @@ struct Box {
 #[derive(Deserialize, Debug)]
 struct Part {
     kind: String,
-    content: String
+    content: String,
 }
 
 fn main() {
+    // Look, this is really goofy probably, maybe I could go about it a better way.. but it works!!
+    let mut path_fancy_names: HashMap<String, String> = HashMap::new();
+
     // Clear the output path
     fs::remove_dir_all("output/").unwrap();
     fs::create_dir("output/").unwrap();
 
-    let template = fs::read_to_string("res/template.html")
+    let html_template = fs::read_to_string("res/template.html")
         .expect("Should have been able to read the file");
 
-    for entry in WalkDir::new("res/pages").into_iter().filter_map(|e| e.ok()).filter(|d| if let Some(e) = d.path().extension() { e == "toml" } else {false}) {
-        let directory_path = &entry.path().parent().to_owned().unwrap().to_str().unwrap()[9..];
-        // Create the directories
-        fs::create_dir_all("output/".to_owned() + &directory_path);
+    for entry in WalkDir::new("res/pages").into_iter().filter_map(|e| e.ok()) {
+        // Skip the entry if there's an error in the metadata
+        if entry.metadata().is_err() {
+            println!("Error reading metadata of file {:?}!!!! Q///Q", entry.path());
+            continue;
+        }
+
+        let directory_path = &entry.path().clone().to_str().unwrap()[9..];
+        let d_path_split: Vec<&str> = directory_path.split("\\").collect();
+
+        let metadata = entry.metadata().unwrap();
+        // If the entry is a directory, create that directory in the output folder
+        if metadata.is_dir() {
+            path_fancy_names.insert(directory_path.to_string(), titlecase(&d_path_split[d_path_split.len()-1].replace("-", " ")));
+
+            fs::create_dir_all("output/".to_owned() + directory_path).ok();
+            continue;
+        }
+        // If the entry is a file that isn't gonna be parsed, copy it over
+        if !directory_path.ends_with("index.toml") {
+            fs::copy(entry.path(), "output/".to_owned() + directory_path).ok();
+            continue;
+        }
+        // If we're dealing with a file needing to be parsed...
         // Read the toml file
+
         let contents = fs::read_to_string(entry.path())
             .expect("Should have been able to read the file");
 
         let page: Page = toml::from_str(&contents).unwrap();
+
+        path_fancy_names.insert(d_path_split[..d_path_split.len()-1].join("\\").to_string(), page.meta.title.to_owned());
 
         let mut page_content = String::new();
         for b in page.boxes {
@@ -59,32 +88,35 @@ fn main() {
         // Options for markdown parser
         let mut options = ComrakOptions::default();
         options.render.unsafe_ = true;
-
+        options.extension.strikethrough = true;
         
-        let markdown = markdown_to_html(&page_content, &options);
+        let markdown: String = markdown_to_html(&page_content, &options);
         
-        let mut navbar = String::from("<div id=\"topnav\"><ul class=\"breadcrumb\"><li>‎</li></ul>\n</div>");
-        if directory_path != "" {
+        // Create the navigation bar / breadcrumbs
+        let mut navbar = String::from("<div id=\"topnav\"><ul class=\"breadcrumb\"><li>Welcome to my website!</li></ul>\n</div>");
+        if directory_path != "\\index.toml" {
             navbar = String::from("<div id=\"topnav\">\n<ul class=\"breadcrumb\">\n");
-            let d_path_split: Vec<&str> = directory_path.split("\\").collect();
             let mut j = 2;
-            for &i in &d_path_split[..d_path_split.len()-1] {
-                if i == "" { navbar += "<li><a href=\"/output/index.html\">Home</a></li>"; continue; }
-                let link = "<li><a href=\"/output".to_owned() + &d_path_split[0..j].join("/") + "/index.html\">" + &i + "</a></li>";
+            for &i in &d_path_split[..d_path_split.len()-2] {
+                if i == "" { navbar += "<li><a href=\"/output/\">Home</a></li>"; continue; }
+                let link = "<li><a href=\"/output".to_owned() + &d_path_split[0..j].join("/") + "/\">" + path_fancy_names.get(&d_path_split[..j].join("\\").to_string()).unwrap_or(&"Q//Q".to_owned()) + "</a></li>";
                 navbar += &link;
                 j+=1;
             }
             navbar += &("<li>".to_owned() + &page.meta.title + "</li>\n</ul></div>");
         }
         
-        fs::write("output/".to_owned() + &directory_path + "/index.html", 
-            template
+        fs::write("output/".to_owned() + &directory_path[..directory_path.len()-10] + "/index.html", 
+            html_template
             .replace("{content}", &markdown)
             .replace("{title}", &page.meta.title)
             .replace("{description}", &page.meta.description)
             .replace("{author}", &page.meta.author)
             .replace("{background}", &page.meta.background)
-            .replace("{topnav}", &navbar))
+            .replace("{topnav}", &navbar)
+            //.replace("<img", "<span class=\"imgborder\"><img")
+            .replace("<imgrs50><img", "<img style='width: 50%; height: auto;'")
+            .replace("<imgrsorig><img", "<img style='width: auto; height: auto;'"))
             .expect("Unable to write file");
     }
 }
